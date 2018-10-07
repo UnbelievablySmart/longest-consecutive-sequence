62208eb39a21a4a1f80248f2d7b00c1e07a89244
Unbelievable <slavlen1999@gmail.com>
2018-10-07 19:36:41 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  //we need to sort array so that its elements are arranged in ascending order
+  array.sort((a, b) => a - b);
+
+  let count = 1;
+  let temporaryElement = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i + 1] == array[i]) {
+      continue;
+    } else if (array[i + 1] == array[i] + 1) {
+      temporaryElement++;
+    } else {
+      while (temporaryElement > 1) {
+        if (count < temporaryElement) {
+          count = temporaryElement;
+        }
+        temporaryElement = 1;
+      }
+    }
+  }
+
+  if (array.length == 0) return 0;
+
+  return count;
 }
